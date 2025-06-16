ca6cad36630aedd211f95fbe444bd25cd9a87878
yashwanthkolli <yashwanthkolli3271@gmail.com>
2025-06-16 16:28:40 +0530

    Shipping policy and terms and conditions

--- src/Pages/Policy/ReturnPolicy.js
+++ src/Pages/Policy/ReturnPolicy.js
@@ -23,21 +23,58 @@ const ReturnPolicy = () => {
         <p><b>1.  IF SHIPMENT IS DAMAGED</b></p>
         <p>Buyer can initiate the Refund Request if the Shipment is damaged and detected after the delivery ("Damaged Shipment"). For the Return Request for Damaged Shipment:</p>
         <p>
-          • The Buyer shall raise a Return Request within 10 (ten) days from the date of delivery.<br />
-          • At the time of making a Return Request to our customer support team, the Buyer will be required to provide appropriate supporting documentation/proof including but not limited to images and videos of the damaged goods, copy of bill and tax invoice.<br />
-          • We will assign the reverse pickup of the Product and once the Product is returned to our facility at no cost to the Buyer, we will initiate an inspection of the Shipment within 48 (forty eight) hours of pick up to confirm the damage. If damage is confirmed, confirmation will be sent to the Buyer acknowledging the return process.<br />
-          • The refund process will begin once the returned Shipment has been inspected and deemed valid for return due to damage. If the Buyer is a credit Buyer, the refund amount will be adjusted against any outstanding credit or dues owed to us and if there is no credit then/after the refund amount will be credited to Buyer's bank account.<br />
-          • If the product do not match the reverse pickup (RVP) details, udaan will not approve return request.
+           • The Buyer shall raise a Return Request within 10 (ten) days from the date of delivery.<br />
+           • At the time of making a Return Request to our customer support team, the Buyer will be required to provide appropriate supporting documentation/proof including but not limited to images and videos of the damaged goods, copy of bill and tax invoice.<br />
+           • We will assign the reverse pickup of the Product and once the Product is returned to our facility at no cost to the Buyer, we will initiate an inspection of the Shipment within 48 (forty eight) hours of pick up to confirm the damage. If damage is confirmed, confirmation will be sent to the Buyer acknowledging the return process.<br />
+           • The refund process will begin once the returned Shipment has been inspected and deemed valid for return due to damage. If the Buyer is a credit Buyer, the refund amount will be adjusted against any outstanding credit or dues owed to us and if there is no credit then/after the refund amount will be credited to Buyer's bank account.<br />
+           • If the product do not match the reverse pickup (RVP) details, RH will not approve return request.
         </p>
         <br />
         <p><b>2.  IN CASE OF QUALITY ISSUES</b></p>
-        <p></p>
+        <p>Buyer can initiate the Refund Request if the Products in the Shipment have quality issues. For the Return Request of Products having quality issues:</p>
+        <p>
+           • The Buyer shall provide details stating the quality issues in the Shipment such as worms, smell, color, incorrect cooking instructions, change in size, wetness, and/or mixed products in the Return Request to our customer support team.<br />
+           • The Buyer shall raise a Return Request within 10 (ten) days from the date of delivery.
+           • At the time of making a Return Request to our customer support team, the Buyer will be required to provide appropriate supporting documentation/proof including but not limited to images and videos of the damaged goods, copy of bill and tax invoice.<br />
+           • We will assign the reverse pickup of the Product and once the Product is returned to our facility at no cost to the Buyer, we will inspect the return shipment within 48 (forty eight) hours to confirm the quality issues. If such quality issues are confirmed, a confirmation will be sent to the Buyer acknowledging the return process.<br />
+           • The refund process will begin once the returned Shipment has been inspected and deemed valid for return due to damage. If the Buyer is a credit Buyer, the refund amount will be adjusted against any outstanding credit or dues owed to us and if there is no credit then/after the refund amount will be credited to Buyer's bank account.<br />
+           • If the product do not match the reverse pickup (RVP) details, RH will not approve return request.
+        </p>
         <br />
         <p><b>3.  EXPIRY RELATED ISSUE</b></p>
-        <p></p>
+        <p>Buyer can initiate the Refund Request if the Products in the Shipment have expiry related issues. For the Return Request for Products having expiry issues:</p>
+        <p>
+           • The Buyer shall provide details stating the expiry related issues of the products in the Return Request to our customer support team. <br />
+           • The Buyer shall raise a Return Request within 10 (ten) days from the date of delivery.
+           • At the time of making a Return Request to our customer support team, the Buyer will be required to provide appropriate supporting documentation/proof including but not limited to images and videos of the damaged goods, copy of bill and tax invoice.<br />
+           • We will assign the reverse pickup of the Product and once the Product is returned to our facility at no cost to the Buyer, we will inspect the return shipment within 48 (forty eight) hours to confirm the quality issues. If such quality issues are confirmed, a confirmation will be sent to the Buyer acknowledging the return process.<br />
+           • The refund process will begin once the returned Shipment has been inspected and deemed valid for return due to damage. If the Buyer is a credit Buyer, the refund amount will be adjusted against any outstanding credit or dues owed to us and if there is no credit then/after the refund amount will be credited to Buyer's bank account.<br />
+           • If the product do not match the reverse pickup (RVP) details, RH will not approve return request.
+        </p>
         <br />
-        <p><b>4.  OTHER ISSUES</b></p><p></p>
+        <p><b>4.  OTHER ISSUES</b></p>
+        <p>In case the items are missing/wrong product is received, the Buyer shall raise a Return Request within 7 (seven) days from the date of delivery</p>
+        <p>We will assign the reverse pick-up of the Product and once the Product is returned to our facility, we will inspect the return shipment within 48 (forty eight) hours to confirm the issues concerned. If the concerned issues are confirmed, confirmation will be sent to the Buyer acknowledging the return process.</p>
+        <p>The refund process will begin once the returned Shipment has been inspected and deemed valid for return due to damage. If the Buyer is a credit Buyer, the refund amount will be adjusted against any outstanding credit or dues owed to us and if there is no credit then/after the refund amount will be credited to Buyer's bank account.<br /></p>
+        <p>Upon receipt of a Return Request from the Buyer, the same will be displayed on the Platform. We will internally scrutinize the Return Request raised by the Buyer. Further, the Return Request raised by the Buyer will either be approved or rejected and the same will be communicated to the Buyer.</p>
         <br />
+        <p><b>5. DECISION ON THE RETURN REQUESTS</b></p>
+        <p>
+          Basis our internal scrutiny of the Return Request raised by the Buyer, any of the following may occur:<br />
+          <br />
+          a. Approval of the Return Request: The Return Request shall be approved upon thorough review and verification in accordance with this Policy, the request may be approved at RH's sole discretion, if sufficient supporting documents are furnished as detailed in Paragraph I, depending on the claim.<br />
+          Upon approval, the claim process will be initiated, and the Buyer will be provided with the necessary instructions for proceeding with the return in compliance with this Policy.<br />
+          To ensure successful return pick up of the Shipment, the Buyer is required to return the complete Shipment in the original condition and in the same bag as it was received in.<br />
+          <br />
+          b. Rejection of the Return Request: The Return Request raised by Buyer shall be rejected, in case of any of the following reasons: (i) If the Buyer does not respond to the inquiry calls and/or communication via other modes made by us to procure missing documentation and information in relation to the Return Request; (ii) supporting documents are insufficient, or Buyer is unable to provide sufficient proof in support of the claim; (iii) If the Buyer's return history shows excessive returns, then the Return Request of the Buyer may be rejected; or (iv) Buyer's Return Request shall automatically stand cancelled in case the Products handed over to us do not pass the verification checks. We will not entertain any further Return Request in case of any cancellation due to verification check.<br />
+          <br />
+          c. Exceptional cases: In exceptional cases such as fraud, deficiency in service, or any other circumstance which may affect the user experience on the Platform, we may on a good faith basis and in our sole discretion decide to pay the transaction amount for the disputed Product. The said payment of transaction amount shall be made to compensate for the inappropriate user experience. In such exceptional cases, payment will be initiated to the Buyer within forty-eight (48) banking hours from: (i) the date of approval of the Return Request by us. <br />
+          <br />
+          In all the above-mentioned cases wherein, the Return Requests are approved by udaan, refund will be initiated to the Buyer as per the terms in this Policy accordingly.
+        </p>
+        <p><b>5. REJECTION AT THE TIME OF DELIVERY</b></p>
+        <p>If the Shipment is damaged, the Buyer may be entitled to reject and return the Shipment at the time of delivery: (i) The Buyer must immediately notify the delivery personnel and clearly state the rejection of the Shipment. Any visible damages or discrepancies with the Shipment must be reported before signing the delivery receipt; (ii) The Buyer must have signed and have mentioned the nature of the damage or issue on the delivery receipt, mentioning the reason for rejection is clearly noted. </p>
+        <p>The damaged Products will be returned to our facility by the delivery personnel at no cost to the Buyer and we will initiate an inspection of the Products within 48 (forty eight) hours. If damage is confirmed, confirmation will be sent to the Buyer acknowledging the Products as undelivered shipment and the refund will be credited to the original payment method used by the Buyer within 7 (seven) working days.</p>
       </div>
       <ContactUsSection />
     </div>
--- src/Pages/Policy/ShippingPolicy.js
+++ src/Pages/Policy/ShippingPolicy.js
@@ -1,10 +1,56 @@
 import React from 'react'
+import ContactUsSection from '../../Components/ContactUsSection/ContactUsSection'
 
 const ShippingPolicy = () => {
   return (
     <div className='policy-page'>
       <h1 className='title'>Shipping Policy</h1>
-      
+      <div className='text'>
+        <p>This Shipping Policy ("<strong>Policy</strong>") is applicable to Undelivered Shipments. This Policy applies to all Orders placed with RiceHouse (herein after referred to as '<strong>RH</strong>' or '<strong>us</strong>' or '<strong>our</strong>' or '<strong>we</strong>') by you ("<strong>User</strong>", "<strong>you</strong>", "<strong>Buyer</strong>" or "<strong>your</strong>") through <a href="/">www.ricehouse.in</a> website and RiceHouse mobile application ("<strong>Platform</strong>").</p>
+        <p>Any capitalized terms used herein this Policy and not defined explicitly shall have the same meaning as defined in the Terms of Use, as may be amended from time to time.</p>
+        <p>We may modify this Policy from time to time, and any such changes will be reflected on the Platform and is effective immediately upon the changes being reflected on the Platform. If you do not agree to the terms contained in this Policy, you are advised not to accept the Terms of Use and this Policy and may forthwith leave and stop using the Platform. Users agree to be bound to any such changes or modifications and understand and accept the importance of regularly reviewing this Policy as updated on the Platform from time to time.</p>
+        <h2>I. Delivery of Shipment to Buyer:</h2>
+        <br />
+        <ol>
+          <li>
+            <p>We or our third-party logistics partner will attempt to deliver the Shipment to the Buyer for a maximum of 1 (one) attempt. However, in case of the occurrence of any one of the following event during the first delivery attempt, we may make no more than one (1) attempt to deliver the Shipment:</p>
+            <p>a. Address of the Consignee is not found to deliver the Shipment during the first attempt; or</p>
+            <p>b. The Buyer has provided incorrect or incomplete shipping details.</p>
+          </li>
+          <li>
+            <p>Despite our reasonable efforts to deliver the Shipment to the Buyer, in some cases, there may be a delivery failure for various reasons, including without limitation:</p>
+            <p>a. Consignee refuses to take delivery of the Shipment;</p>
+            <p>b. Consignee is not reachable/ unavailable at the delivery address to receive the Shipment;</p>
+            <p>c. Consignee does not have enough cash to make payment for the Shipment; and/or</p>
+            <p>d. Consignee requests for future delivery attempts.</p>
+          </li>
+        </ol>
+        <p>(the Shipment in each of the above cases shall be referred to as "<strong>Undelivered Shipment</strong>")</p>
+        <ol start="3">
+          <li>In certain exceptional scenarios, or where the delivery location is in a non-serviceable zone, i.e., where we are not able to deliver due to unforeseen circumstances or service unavailability, for such Shipment we will not attempt delivery to Buyer, and such Shipment shall also be deemed to be an Undelivered Shipment for the purposes of this Policy.</li>
+        </ol>
+        <br />
+        <h2>II. Process of Refund to the Buyer:</h2>
+        <br />
+        <ol>
+        <li>
+        <p>If the Buyer has made the payment in advance for the Products/ Services purchased at the time of placing the Order on the Platform and the Shipment is marked as Undelivered Shipment, the refund will be processed through the same channel of payment which was used by the Buyer to make the payment within 2 (two) business days;</p>
+        </li>
+        <li>
+        <p>If the refund is not processed to the Buyer within 2 (two) working days, the Buyer can connect with our 'Customer Support' and report the concern;</p>
+        </li>
+        <li>
+        <p>udaan will conduct a thorough investigation with our shipping partner to determine the cause of the delay or failure;</p>
+        </li>
+        <li>
+        <p>A full refund will be processed to the Buyer's Account within 2 (two) working days from the date of refund request raised by the Buyer through the same channel of payment which was used by the Buyer to make the payment.</p>
+        </li>
+        </ol>
+        <h2>III. Return and Refund of Products</h2>
+        <br />
+        <p>Return and Refund of Shipments shall be dealt with in accordance with Return Shipment Policy available here.</p>
+      </div>
+      <ContactUsSection />
     </div>
   )
 }
